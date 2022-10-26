980a7c8734df958c4cdc5ad6cb3a759e6dc041d8
Matheus Leal <matheusaquino@id.uff.br>
2022-10-25 22:02:09 -0300

    lint: run lint

--- src/utils/encryptionUtils.ts
+++ src/utils/encryptionUtils.ts
@@ -1,22 +1,22 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 
-export function generateHash({ password, saltRounds = 10 }: { password: string, saltRounds?: number}): string {
-  return bcrypt.hashSync(password, saltRounds);
+export function generateHash ({ password, saltRounds = 10 }: { password: string, saltRounds?: number }): string {
+  return bcrypt.hashSync(password, saltRounds)
 }
 
-export function verifyHash({ password, hash }: { password: string; hash: string }): boolean {
-  return bcrypt.compareSync(password, hash);
+export function verifyHash ({ password, hash }: { password: string, hash: string }): boolean {
+  return bcrypt.compareSync(password, hash)
 }
 
-export async function generateCookie({ key, value }: { key: string; value: string }) {
-  const data: { [key: string]: string } = {};
-  data[key] = value;
+export async function generateCookie ({ key, value }: { key: string, value: string }): Promise<string> {
+  const data: { [key: string]: string } = {}
+  data[key] = value
   return jwt.sign({ data }, process.env.JWT_SECRET, {
-    expiresIn: process.env.USER_COOKIE_EXPIRY,
-  });
+    expiresIn: process.env.USER_COOKIE_EXPIRY
+  })
 }
 
-export async function verifyCookie({ token }: { token: string }) : Promise<any> {
-  return jwt.verify(token, process.env.JWT_SECRET);
+export async function verifyCookie ({ token }: { token: string }): Promise<any> {
+  return jwt.verify(token, process.env.JWT_SECRET)
 }
