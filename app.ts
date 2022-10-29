823c3b16ada800bf4931589b58481a34bdcf1ffc
Matheus Leal <matheusaquino@id.uff.br>
2022-10-28 22:54:44 -0300

    lint: run on app.ts

--- app.ts
+++ app.ts
@@ -1,13 +1,13 @@
-import 'reflect-metadata';
-import express from 'express';
-import { AppDataSource } from "./src/data-source"
+import 'reflect-metadata'
+import express from 'express'
+import { AppDataSource } from './src/data-source'
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 AppDataSource.initialize().then(async () => {
-  console.log('connection to database');
+  console.log('connection to database')
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
-}).catch(error => console.log(error));
+    console.log(`App listening on port ${port}`)
+  })
+}).catch(error => console.log(error))
