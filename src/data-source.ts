0f6b26fe6ba8fc3cc7613ab82f73fbc9097c6677
Matheus Leal <matheusaquino@id.uff.br>
2022-10-28 22:55:08 -0300

    lint

--- src/data-source.ts
+++ src/data-source.ts
@@ -3,15 +3,15 @@ import { DataSource } from 'typeorm'
 import { User } from './entity/user.entity'
 
 export const AppDataSource = new DataSource({
-    type: 'mysql',
-    host: process.env.DATABASE_MYSQL_HOST,
-    port: parseInt(process.env.DATABASE_MYSQL_PORT, 10),
-    username: process.env.DATABASE_MYSQL_USERNAME,
-    password: process.env.DATABASE_MYSQL_PASSWORD,
-    database: process.env.DATABASE_MYSQL_NAME,
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-});
+  type: 'mysql',
+  host: process.env.DATABASE_MYSQL_HOST,
+  port: parseInt(process.env.DATABASE_MYSQL_PORT, 10),
+  username: process.env.DATABASE_MYSQL_USERNAME,
+  password: process.env.DATABASE_MYSQL_PASSWORD,
+  database: process.env.DATABASE_MYSQL_NAME,
+  synchronize: true,
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: []
+})
