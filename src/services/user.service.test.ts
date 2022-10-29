299c239fce372c9dd761d0140a068be530cee87a
Matheus Leal <matheusaquino@id.uff.br>
2022-10-28 22:55:50 -0300

    test: test case with mysql database

--- src/services/user.service.test.ts
+++ src/services/user.service.test.ts
@@ -1,26 +1,43 @@
+import { AppDataSource } from '../data-source'
+
+import UserRepositoryMysql from '../repository/mysql/user.repository.mysql'
+
+import { User } from '../entity/user.entity'
+
 import UserService from './user.service'
-import UserRepositoryMemory from '../repository/memory/user.repository.memory'
+
+beforeAll(async () => {
+  await AppDataSource.initialize()
+})
+
+afterAll(async () => {
+  const userRepository = AppDataSource.getRepository(User)
+  await userRepository.clear()
+  await AppDataSource.destroy()
+})
 
 describe('createUser', () => {
   test('when receive correctly data then save', async () => {
-    const userRepositoryMemory = new UserRepositoryMemory()
+    const userRepositoryMysql = new UserRepositoryMysql()
 
     const userService = new UserService({
-      userRepository: userRepositoryMemory
+      userRepository: userRepositoryMysql
     })
 
-    const user = await userService.createUser({
-      birthday: new Date(),
-      email: 'matheusleal.a@gmail.com',
-      firstName: 'Matheus',
-      lastName: 'Leal',
-      gender: 'male',
-      password: '12346',
-      relationshipStatus: 'married'
-    })
+    const user = new User()
 
-    expect(user.id).toBeDefined()
-    expect(user.createdAt).toBeDefined()
-    expect(user.updatedAt).toBeDefined()
+    user.birthday = new Date()
+    user.email = 'matheusleal.a@gmail.com'
+    user.firstName = 'Matheus'
+    user.lastName = 'Leal'
+    user.gender = 'male'
+    user.password = '123456'
+    user.relationshipStatus = 'married'
+
+    const output = await userService.createUser(user)
+
+    expect(output.id).toBeDefined()
+    expect(output.createdAt).toBeDefined()
+    expect(output.updatedAt).toBeDefined()
   })
 })
