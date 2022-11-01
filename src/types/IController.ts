72db2f1f4956c0f642d027dba56eea701728e0a5
Matheus Leal <matheusaquino@id.uff.br>
2022-10-31 22:50:55 -0300

    feat: creation of controllers for signup and login user

--- src/types/IController.ts
+++ src/types/IController.ts
@@ -4,4 +4,6 @@ import IRequest from './IRequest'
 export default interface IController {
   // eslint-disable-next-line @typescript-eslint/prefer-function-type
   (req: IRequest, res: Response): void
+  data?: any
+  success?: boolean
 };
